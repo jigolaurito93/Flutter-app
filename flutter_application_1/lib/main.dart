b5cff78744c6d5600f47c5f556964731bacabf66
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-07 06:02:15 -0500

    scaffolding widgets

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Hello World!!!'),
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://www.basketballnetwork.net/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1290/MTk5NjcyODc5MjY3MTk0NDk2/kobe-bryant-1.jpg'),
+          ),
+        ),
       ),
     ),
   );
