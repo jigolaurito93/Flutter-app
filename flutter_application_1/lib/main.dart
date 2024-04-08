9bcafb8cd212d72db849606b39d86e94b43a97c9
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-08 02:22:36 -0500

    importing images to assets

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,13 +6,12 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey,
         appBar: AppBar(
-          title: const Text('I Am Rich'),
+          title: const Text('I Am Rich Boy'),
           backgroundColor: Colors.blueGrey[900],
         ),
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://www.basketballnetwork.net/.image/c_fit%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1290/MTk5NjcyODc5MjY3MTk0NDk2/kobe-bryant-1.jpg'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
